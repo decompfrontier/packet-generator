c164b2665ffc15fb62f9b40a7a56b44813502799
Arves100 <4847635+arves100@users.noreply.github.com>
2025-09-09 23:04:55 +0200

    Add Unit MST

--- data/mst/arena.py
+++ data/mst/arena.py
@@ -7,7 +7,7 @@ class ArenaRewardTypes:
 
 
 @keyjson(key_group = "6kWq78zx", array=ArrayStep.Array)
-class ArenaRankMst:
+class ArenaRankMst: # F_ARENA_MST
     """Rankings of the PVP Arena."""
 
     id = { "JmFn3g9t": intstr, "doc": "Ranking ID" }
@@ -15,5 +15,5 @@ class ArenaRankMst:
     rank_point_start = { "w0aTd94Y": intstr, "doc": "Points when this rank starts (included)" }
     rank_point_end = { "1U3eBCyY": intstr, "doc": "Points when this rank ends (included)" }
     reward_type = { "IkmC8gG2": intstr, "doc": "Type of reward" } # TODO: discover and make an Enum with str-types !!
-    reward_param = { "empaR60j": str, "doc": "Configuration of the reward" } 
+    reward_param = { "empaR60j": str, "doc": "Configuration of the reward" } # list[int]: ArrayType.DOUBLEDOT
     scenario_info = { "N4XVE1uA": str, "doc": "ID of the scene to play" }
