ce0ede58d614bdcb959501f503f65f09957798ce
Arves100 <4847635+arves100@users.noreply.github.com>
2026-03-02 13:27:01 +0100

    runtime: Refactor for multi runtime support in c++

---
+++ runtime/cpp/unit_tests/test_glaze_single_array.cpp
@@ -0,0 +1,42 @@
+#include <pkgen_glaze_helpers.hpp>
+#include <gtest/gtest.h>
+
+struct Ciao {
+	std::string a;
+};
+
+struct CiaoArray {
+	Ciao data;
+};
+
+template <>
+struct glz::meta<Ciao> {
+	using T = Ciao;
+	constexpr static auto value = object("_a_", &T::a);
+};
+
+template <>
+struct glz::meta<CiaoArray> {
+	using T = CiaoArray;
+	constexpr static auto value = object(
+		"ciao", single_array<&T::data>()
+	);
+};
+
+TEST(glaze, single_array) {
+	const auto input = R"({"ciao": [{"_a_": "qualcosa"}]})";
+	const auto ec = glz::read_json<CiaoArray>(input);
+	ASSERT_TRUE(ec.has_value());
+	const auto& ecval = ec.value();
+	ASSERT_EQ(ecval.data.a, "qualcosa");
+
+	std::string buffer;
+	const auto& tc = ec.value();
+	const auto ec2 = glz::write_json(tc, buffer);
+	ASSERT_FALSE(ec2);
+	ASSERT_EQ(buffer, input);
+
+	const auto broken = R"({"ciao": [{"_a_": "uno"},{"_a"_: "due"}]})";
+	const auto ec3 = glz::read_json<CiaoArray>(input);
+	ASSERT_FALSE(ec.has_value());
+}
